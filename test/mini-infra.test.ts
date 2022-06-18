87096c088d15ad25ff3c8e9ab41586c2b99e733e
haneimo <haneimo@gmail.com>
2022-06-18 22:04:15 +0900

    Initial commit

---
+++ test/mini-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MiniInfra from '../lib/mini-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mini-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MiniInfra.MiniInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
